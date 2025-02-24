ad26103577fb14da8d7e4e3a149d6a720646a0df
Tushar Sharma <tsharma.hotbitinfosoft.com>
2025-02-24 12:35:20 +0530

    added navbar feature

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,13 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { buttonProps } from './index';
+
+const CustomButton=({variant='outlined',styles={},name='button'}:buttonProps)=> {
+    return (
+        <Stack spacing={2} direction="row">
+            <Button sx={styles} variant={variant}>{name}</Button>
+        </Stack>
+    );
+}
+
+export default CustomButton
